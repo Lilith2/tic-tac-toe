50711652e12c2fbec5f0c8a9789b43a9e9e78b05
Lilith2 <56748415+Lilith2@users.noreply.github.com>
2020-12-04 03:35:04 -0800

    fixed baseboard print & added comments
    Realigned the baseboard print and added some comments to explain the code.

--- main.c
+++ main.c
@@ -4,36 +4,25 @@
 #include <math.h>
 #include <time.h>
 
-
-bool checkRunning(bool state) // not used yet
+bool checkRunning(bool running)
 {
-    if(state == true)
+    if(running == true)
     {
       return true;
     }else{return false;};
 }
 
-int checkTurns(int turns)
-{
-  if(turns <= 9)
-  {
-    return turns;
-  }else{return 0;};
-}
-
+/*
+return a pointer to the board of tic tac toe. We will modify chars stored at each index
+location with our player's & computers move choices.
+*/
 char * returnBaseBoard()
 {
   static char baseBoard[9] = {'1','2','3','4','5','6','7','8','9'};
   return baseBoard;
 }
-
-char * returnInteractiveBoard()
-{
-  static char interactiveBoard[9];
-  return interactiveBoard;
-}
-
-int init() //setup game
+//setup the game icons
+int init()
 {
   printf("Do you want to be X or O?\n");
   char player;
@@ -47,27 +36,28 @@ int init() //setup game
     return 0;
   }else{return 2;};
 }
-
+//print the default static messages each game
 void announcments()
 {
   printf("Tic-Tac-Toe\n");
-  printf("Code by Mark McDonald\n");    printf("You will be asked if you want to be X or O.\n");
+  printf("Code by Mark McDonald\n");    
+  printf("You will be asked if you want to be X or O.\n");
   printf("A new game will then start.\n");
   printf("A heads/tails will decide who goes first.\n");
   printf("You make your selections using numbers: \n");
   printf("1 | 2 | 3\n");
-  printf("---+---+---\n");
+  printf("--+---+---\n");
   printf("4 | 5 | 6\n");
-  printf("---+---+---\n");
+  printf("--+---+---\n");
   printf("7 | 8 | 9\n");
 }
-
+//function to print the board each time we update it on the game loop
 void printBoard(char * baseBoard)
 {
   printf(" %c | %c | %c", baseBoard[0], baseBoard[1], baseBoard[2]);
-  printf("\n ---+---+---\n");
+  printf("\n --+---+--\n");
   printf(" %c | %c | %c", baseBoard[3], baseBoard[4], baseBoard[5]);
-  printf("\n ---+---+---\n");
+  printf("\n --+---+--\n");
   printf(" %c | %c | %c\n", baseBoard[6], baseBoard[7], baseBoard[8]);
   printf("\n");
 }
@@ -79,8 +69,34 @@ typedef enum
   WIN_TYPE_NOBODY,
 } win_t;
 
-int computerLogic(int turn)
+win_t check_winner(char symbol)
 {
+  //puts("TODO: Check winner")
+  char * game_brd = returnBaseBoard();
+  if( /* Horizontatl Win */
+    (symbol == game_brd[0] && game_brd[0] == game_brd[1] && game_brd[1] == game_brd[2]) ||
+    (symbol == game_brd[3] && game_brd[3] == game_brd[4] && game_brd[4] == game_brd[5]) ||
+    (symbol == game_brd[6] && game_brd[6] == game_brd[7] && game_brd[7] == game_brd[8]) ||
+    //testing custom win condition
+    (symbol == game_brd[6] && symbol == game_brd[7] && symbol == game_brd[8]) ||
+    /* Vertical Win */
+    (symbol == game_brd[0] && game_brd[0] == game_brd[3] && game_brd[3] == game_brd[6]) ||
+    (symbol == game_brd[1] && game_brd[1] == game_brd[4] && game_brd[4] == game_brd[7]) ||
+    (symbol == game_brd[2] && game_brd[2] == game_brd[5] && game_brd[5] == game_brd[8]) ||
+    /* Diagonal Win */
+    (symbol == game_brd[0] && game_brd[0] == game_brd[4] && game_brd[4] == game_brd[8]) ||
+    (symbol == game_brd[2] && game_brd[2] == game_brd[4] && game_brd[4] == game_brd[6])
+    )
+    {
+      return WIN_TYPE_WIN; //We have a winner!
+    }
+    else{return WIN_TYPE_NOBODY;}
+      
+  }
+//controls all logic for the computer to choose grids
+int computerLogic(int turn , char playerIcon)
+{
+  char * baseBoard = returnBaseBoard(); //creating a local scope pointer to the original board array
   int computerChoices[9];
   bool valid = false;
   int computerIndex;
@@ -95,26 +111,32 @@ int computerLogic(int turn)
         matches++;
       }
     }
+    if(baseBoard[computerIndex] == playerIcon)
+    {
+      matches++;
+    }
     if(matches == 0)
     {
       valid = true;
     }
   }
-  computerChoices[turn] = computerIndex;
-  return computerIndex;
+  if(valid)
+  {
+    computerChoices[turn] = computerIndex;
+    return computerIndex;
+  }
+  return 20; // the number itself makes no difference long as it's outside the range of the array.
+  //we use it to signify that the computer's choice is invalid
 }
-
-void playerLogic(char * baseBoard[9], char * playerIcon)
+// gets the player's choice for grid
+int playerLogic()
 {
   int playerIndex;
   int answer;
-  char icon = *playerIcon;
-    printBoard(*baseBoard);
-    scanf(" %d" , &answer);
-    if(answer == 1)
-      baseBoard[0] = &icon;
-    else{baseBoard[answer - 1] = &icon;};
-    printBoard(*baseBoard);
+  scanf(" %d" , &answer);
+  if(answer == 1)
+    return 0;
+  else{return playerIndex = answer - 1;};
 }
 
 int main(void) 
@@ -130,6 +152,8 @@ int main(void)
     {
       playerIcon = 'O';
       cpuIcon = 'X';
+      printf("Player is O\n");
+      printf("Computer is X\n");
       notpicked = false;
     }
     else if(player == 1)
@@ -144,38 +168,107 @@ int main(void)
       printf("You have selected an invalid answer.\n");
     }
   }
+  bool running = true;
   bool state = true;
+  int playerScore = 0;
+  int computerScore = 0;
   int turns = 9;
   int cpuTurn = 0;
-  while(state == true)
+  while(checkRunning(running)) //main gameplay loop
   {
-    time_t now = time(NULL);
-    srand(now);
-    int first = rand() % 3;
-    char * baseBoard = returnBaseBoard();
-    if(first >= 2)
+    while(state == true)
     {
-      printf("Player goes first!\n");
-      int answer;
+      time_t now = time(NULL);
+      srand(now);
+      int first = rand() % 3;
+      char * baseBoard = returnBaseBoard(); //creating a local scope pointer to the original board array
+      if(first >= 2)
+      {
+        printf("Player goes first!\n");
+        int answer;
+        printBoard(baseBoard);
+        scanf(" %d" , &answer);
+        if(answer == 1)
+          baseBoard[0] = playerIcon;
+        else{baseBoard[answer - 1] = playerIcon;};
+        printBoard(baseBoard);
+        turns--;
+        state = false;
+      }else{printf("Computer goes first!\n");};
+    
+  //int cpuIndex returned to show if computer choice is valid or not & what grid if it's valid
+      int cpuIndex = computerLogic(cpuTurn, playerIcon);
+      if(cpuIndex != 20) 
+        baseBoard[cpuIndex] = cpuIcon;
+        printf("Computer print.\n");
+        printBoard(baseBoard);
+        cpuTurn++;
+        turns--;
+        state = false;
+    }
+    char * baseBoard = returnBaseBoard(); //creating a local scope pointer to the original board array
+    bool win = false;
+    while(turns > 0 && win == false)
+    {
+      printf("player print.\n");
       printBoard(baseBoard);
-      scanf(" %d" , &answer);
-      if(answer == 1)
-        baseBoard[0] = playerIcon;
-      else{baseBoard[answer] = playerIcon;};
-      printBoard(baseBoard);
+      int playerIndex = playerLogic();
+      int cpuIndex = computerLogic(cpuTurn, playerIcon);
+      if(baseBoard[playerIndex] == playerIcon || baseBoard[playerIndex] == cpuIcon)
+      {
+        printf("invalid grid, turned skipped...\n");
+      }
+      else if(baseBoard[cpuIndex] == playerIcon || cpuIndex == 20)
+      {
+        printf("invalid grid, cpu turn skipped...\n");
+      }
+      else {baseBoard[playerIndex] = playerIcon;} //only write the player's move if it's valid
+      if(check_winner(playerIcon) == WIN_TYPE_WIN)
+      {
+        printf("PLAYER WINS!!!!\n");
+        playerScore++;
+        win = true;
+        break;
+      }
       turns--;
-    }else{printf("Computer goes first!\n");};
-//array to store computer choices so it doesn't choose same place 2 times
-    baseBoard[computerLogic(cpuTurn)] = cpuIcon;
-    cpuTurn++;
-    turns--;
+      printf("player print.\n");
+      printBoard(baseBoard);
+      //quick check to make sure the computer still has turns remaining
+      if(cpuTurn < 9) 
+      {
+        baseBoard[cpuIndex] = cpuIcon;
+        if(check_winner(cpuIcon) == WIN_TYPE_WIN)
+        {
+          printf("COMPUTER WINS!!!!\n");
+          computerScore++;
+          win = true;
+          break;
+        }
+        cpuTurn++;
+        turns--;
+        printf("computer print.\n");
+        printBoard(baseBoard);
+      }
+    }
+    printf("FINAL BOARD PRINT.\n");
     printBoard(baseBoard);
-    char * playerIconP = &playerIcon;
-    playerLogic(&baseBoard, playerIconP);
-    printBoard(baseBoard);
-    //add more logic later
-    state = false;
+    printf("GAME ENDED....\n");
+    char input;
+    printf("Play Again? y/n \n"); //get user input to check for replay
+    scanf(" %c", &input);
+    if(input == 'y' || input == 'Y')
+    {
+      char newBoard[9] = {'1','2','3','4','5','6','7','8','9'}; // clear the current board by writing each index to the original numbers
+      for(int i = 0; i < 10; i++)
+      {
+        baseBoard[i] = newBoard[i];
+      }
+      continue;
+    }
+    else if(input == 'n' || input == 'N')
+    {
+      running = false;
+    }
   }
-
-  return 0;
+    return 0;
 }
